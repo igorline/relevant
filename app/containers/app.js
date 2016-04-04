291c1d55c89b85ce484d58256668522d09186a01
jaygoss <jaygoss@gmail.com>
2016-04-04 14:59:01 -0400

    nav & footer

--- app/containers/app.js
+++ app/containers/app.js
@@ -6,41 +6,104 @@ import React, {
   StyleSheet,
   Text,
   View,
-  TextInput
+  TextInput,
+  StatusBarIOS
 } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as authActions from '../actions/authActions';
 var {Router, routerReducer, Route, Container, Animations, Schema} = require('react-native-redux-router');
 var {NavBar, NavBarModal} = require('../components/NavBar');
+var Button = require('react-native-button');
 import Auth from './auth';
 import Import from './import';
 import Profile from './profile';
+import Read from './read';
+import Footer from './footer';
+import Post from './post';
+import Discover from './discover';
+import Nav from './nav';
+
 
 class Application extends Component {
+  componentDidMount() {
+    // StatusBarIOS.setNetworkActivityIndicatorVisible(true);
+
+  }
+
   render() {
     return (
+      <View style={{flex:1}} >
+      <View style={{position:'absolute',left:0,right:0,top:0,bottom:0,backgroundColor:'white'}}/>
+        <Router>
+          <Route name="Auth" component={Auth}  hideNavBar={false} hideFooter={false} type="replace" initial={true} title="Home" />
+          <Route name="SignUp" component={Auth} hideNavBar={false} hideFooter={false} type="replace" title="Sign up" />
+          <Route name="LogIn" component={Auth} hideNavBar={false} hideFooter={false} type="replace" title="Log in" />
+          <Route name="Import" component={Import} hideNavBar={false}  type="replace" title="Import" />
+          <Route name="Profile" component={Profile} hideNavBar={false}  type="replace" title="Profile" />
+          <Route name="Read" component={Read} hideNavBar={false}  type="replace" title="Read" />
+          <Route name="Post" component={Post} hideNavBar={false}  type="replace" title="Post" />
+          <Route name="Discover" component={Discover} hideNavBar={false}  type="replace" title="Discover" />
 
-    <View style={{flex:1}} >
-                <View style={{position:'absolute',left:0,right:0,top:0,bottom:0,backgroundColor:'white'}}/>
-                <Router>
-                    <Schema name="modal" sceneConfig={Animations.FlatFloatFromBottom} navBar={NavBarModal}/>
-                    <Schema name="default" sceneConfig={Animations.FlatFloatFromRight} navBar={NavBar}/>
-                    <Schema name="withoutAnimation" navBar={NavBar}/>
-                    <Schema name="tab" navBar={NavBar}/>
-
-                  <Route name="Auth" component={Auth}  hideNavBar={true} type="replace" initial={true} title="Home" />
-                  <Route name="SignUp" component={Auth} hideNavBar={true} type="replace" title="Sign up" />
-                  <Route name="LogIn" component={Auth} hideNavBar={true}  type="replace" title="Log in" />
-                  <Route name="Import" component={Import} hideNavBar={true}  type="replace" title="Import" />
-                  <Route name="Profile" component={Profile} hideNavBar={true}  type="replace" title="Profile" />
-                </Router>
-
-            </View>
-
+          {/*<Schema name="modal" sceneConfig={Animations.FlatFloatFromBottom} footer={Nav}/>*/}
+          <Schema name="default" sceneConfig={Animations.FlatFloatFromBottom} footer={Footer} navBar={Nav}/>
+          {/*<Schema name="withoutAnimation" footer={Nav}/>*/}
+          {/*<Schema name="tab" footer={Nav}/>*/}
+        </Router>
+      </View>
     );
   }
 }
 
 export default Application
+
+const styles = StyleSheet.create({
+  uploadAvatar: {
+    width: 200,
+    height: 200
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderWidth: 20,
+    borderStyle: 'solid',
+    borderColor: 'transparent'
+  },
+  nav: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3C8AE8'
+  },
+  navItem: {
+    color: 'white'
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  input: {
+    borderColor: '#cccccc',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    height: 30,
+    width: 200,
+    alignSelf: 'center',
+    margin: 5
+  },
+  marginTop: {
+    marginTop: 10
+  }
+});
 
---
+++ app/containers/read.js
@@ -0,0 +1,90 @@
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput
+} from 'react-native';
+import { connect } from 'react-redux';
+var Button = require('react-native-button');
+import { bindActionCreators } from 'redux';
+import * as authActions from '../actions/authActions';
+
+class Read extends Component {
+  componentDidMount() {
+  }
+
+  render() {
+    var self = this;
+
+    return (
+      <View style={styles.container}>
+       <Text>Read</Text>
+      </View>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+    router: state.routerReducer
+   }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(authActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Read)
+
+const styles = StyleSheet.create({
+  uploadAvatar: {
+    width: 200,
+    height: 200
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderWidth: 20,
+    borderStyle: 'solid',
+    borderColor: 'transparent'
+  },
+  wrap: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  input: {
+    borderColor: '#cccccc',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    height: 30,
+    width: 200,
+    alignSelf: 'center',
+    margin: 5
+  },
+  marginTop: {
+    marginTop: 10
+  }
+});
+
