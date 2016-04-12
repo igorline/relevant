d0d0dc3b65385f5712ddaf2ca5ff90e2d612e5cb
Slava 4REAL <slava@4real.io>
2016-04-12 14:33:58 -0400

    do invest

--- app/reducers/posts.js
+++ app/reducers/posts.js
@@ -3,18 +3,21 @@ import * as types from '../actions/actionTypes';
 const initialState = {index: null, userPosts: null, postError: null, activePost: null};
 
 const updatePostElement = (array, post) => {
+  console.log("UPDATING POST")
   var index = array.findIndex(function(el){
     return el._id == post._id
   })
+  console.log(index)
   return [
     ...array.slice(0, index),
     post,
-    ...array.slice(index+1,)
+    ...array.slice(index + 1)
   ]
 }
 
 export default function auth(state = initialState, action) {
   console.log(action.type)
+  console.log(action)
   switch (action.type) {
 
     case types.SET_POSTS: {
@@ -42,13 +45,14 @@ export default function auth(state = initialState, action) {
     }
 
     case types.UPDATE_POST: {
-      return Objext.assign({}, state, {
+      return Object.assign({}, state, {
         'index': updatePostElement(state.index, action.payload)
       })
     }
 
 
     default:
+      console.log(state)
       return state
   }
 };
