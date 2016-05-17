f9eee2e65c0b88ebc952093c0a691d50d1fd5713
jaygoss <jaygoss@gmail.com>
2016-05-17 16:07:18 -0400

    filter ranked posts by tag

--- app/containers/read.container.js
+++ app/containers/read.container.js
@@ -26,7 +26,8 @@ class Read extends Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      page: 0
+      page: 0,
+      tag: null
     }
   }
 
@@ -48,16 +49,47 @@ class Read extends Component {
     self.setState({page: page});
   }
 
+  setTag(tag) {
+    var self = this;
+    self.setState({page: 0, tag: tag});
+  }
+
   render() {
     var self = this;
     var postsEl = null;
     var posts = null;
+    var pages = null;
     var paginationEl = null;
+    var tags = [];
+    var tagsEl = null;
     var page = self.state.page;
+    var taggedPosts = null;
 
     if (self.props.posts.feed) {
-      posts = self.props.posts.feed.slice(page*10, (page*10)+10);
-      var pages = Math.ceil(self.props.posts.feed.length / 10);
+
+      self.props.posts.feed.forEach(function(post, i) {
+        post.tags.forEach(function(tag, j) {
+          tags.push(tag);
+        })
+      })
+      console.log(tags, 'tags')
+      tagsEl = tags.map(function(tag, i) {
+        return (
+          <Text style={styles.tagBox} onPress={self.setTag.bind(self, tag)} key={i}>{tag.name}</Text>
+          )
+      })
+
+      if (!self.state.tag) {
+        posts = self.props.posts.feed.slice(page*10, (page*10)+10);
+        pages = Math.ceil(self.props.posts.feed.length / 10);
+      } else {
+        taggedPosts = [];
+        self.props.posts.feed.forEach(function(post, i) {
+          if (post.tags.indexOf(self.state.tag) > -1) taggedPosts.push(post);
+        })
+        posts = taggedPosts.slice(page*10, (page*10)+10);
+        pages = Math.ceil(taggedPosts.length / 10);;
+      }
 
       paginationEl = [];
       for (var i = 0; i < pages; i++) {
@@ -74,6 +106,7 @@ class Read extends Component {
 
     return (
       <ScrollView style={[styles.readContainer]}>
+      <View style={styles.tags}>{tagsEl}</View>
       <View style={styles.pagination}>{paginationEl}</View>
        {postsEl}
        <View pointerEvents={'none'} style={styles.notificationContainer}>
@@ -103,9 +136,6 @@ export default connect(mapStateToProps, mapDispatchToProps)(Read)
 
 const localStyles = StyleSheet.create({
   readContainer: {
-   // padding: 20,
-   // borderWidth: 1,
-   // borderColor: 'red'
   },
   readHeader: {
     marginBottom: 10
