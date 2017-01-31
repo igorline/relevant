c122da6828d948c5e4994644420d86384adcbdac
Slava 4REAL <slava@4real.io>
2017-01-31 14:22:52 -0500

    revert

--- app/utils/post.js
+++ app/utils/post.js
@@ -17,162 +17,173 @@ function extractDomain(url) {
   return noPrefix;
 }
 
-export function generatePreview(link) {
-  let responseUrl;
+function getYoutubeLink(link) {
+  let videoId = link.split('v=')[1];
+  let ampersandPosition = videoId.indexOf('&');
+  if (ampersandPosition !== -1) {
+    videoId = videoId.substring(0, ampersandPosition);
+  }
 
-  let video_id = null;
-  if (link.indexOf('youtube') > -1) {
-    video_id = link.split('v=')[1];
-    let ampersandPosition = video_id.indexOf('&');
-    if (ampersandPosition !== -1) {
-      video_id = video_id.substring(0, ampersandPosition);
+  // TODO should go in publicenv.js
+  let apiKey = '***REMOVED***';
+  let apiUrl = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + videoId + '&key=' + apiKey;
+
+  return fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'User-Agent': 'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)',
     }
+  })
+  .then((response) => {
+    console.log(response, 'response');
+    return response.json();
+  })
+  .then((responseJSON) => {
+    console.log(responseJSON, 'responseJSON');
+    let image = null;
+    let description = null;
+    let title = null;
+    let videoInfo = null;
 
-    let apiKey = '***REMOVED***';
-    let apiUrl = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + video_id + '&key=' + apiKey;
-    console.log(apiUrl, 'apiUrl');
-
-    return fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-      }
-    })
-    .then((response) => {
-      console.log(response, 'response');
-      return response.json();
-    })
-    .then((responseJSON) => {
-      console.log(responseJSON, 'responseJSON');
-      let image = null;
-      let description = null;
-      let title = null;
-      let videoInfo = null;
-
-      if (responseJSON.items) {
-        if (responseJSON.items[0]) {
-          if (responseJSON.items[0].snippet) {
-            videoInfo = responseJSON.items[0].snippet;
-            if (videoInfo.title) title = videoInfo.title;
-            if (videoInfo.thumbnails) image = videoInfo.thumbnails.high.url;
-            if (videoInfo.description) description = videoInfo.description;
-          }
+    if (responseJSON.items) {
+      if (responseJSON.items[0]) {
+        if (responseJSON.items[0].snippet) {
+          videoInfo = responseJSON.items[0].snippet;
+          if (videoInfo.title) title = videoInfo.title;
+          if (videoInfo.thumbnails) image = videoInfo.thumbnails.high.url;
+          if (videoInfo.description) description = videoInfo.description;
         }
       }
+    }
 
-      let obj = {
-        image,
-        description,
-        title,
-        link
-      };
+    let obj = {
+      image,
+      description,
+      title,
+      link
+    };
 
-      return obj;
-    })
-    .catch((error) => {
-      console.log(error, 'error');
-      return false;
-    });
-  } else {
-    return fetch(link, {
-      method: 'GET',
-      headers: {
-        'User-Agent': 'facebookexternalhit/1.0 (+http://www.facebook.com/externalhit_uatext.php)'
-      }
-    })
-    .then((response) => {
-      responseUrl = response.url;
-      return response.text();
-    })
-    .then((responseTxt) => {
-      const $ = cheerio.load(responseTxt);
+    return obj;
+  })
+  .catch((error) => {
+    console.log(error, 'error');
+    return false;
+  });
+}
 
 
+export function generatePreview(link) {
+  let responseUrl;
+  let fbHeader = {
+    'User-Agent': 'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)',
+  };
 
-      let redirect = $("meta[http-equiv='refresh']")[0];
-      let redirectUrl;
-
-      if (redirect && redirect.attribs && redirect.attribs.content) {
-        redirectUrl = redirect.attribs.content.split('URL=')[1];
-      }
-      if (redirectUrl) return generatePreview(redirectUrl);
-
-      let canonical = $("link[rel='canonical']");
-      if (canonical && canonical.attribs) canonical = canonical.attribs;
-      else canonical = null;
-
-      if (canonical && extractDomain(canonical.href) !== extractDomain(link)) {
-        return generatePreview(canonical.href);
-      }
-
-      let data = {
-        title: null,
-        description: null,
-        image: null,
-        'og:type': null,
-        'og:title': null,
-        'og:description': null,
-        'og:image': null,
-        'og:image:url': null,
-        'twitter:title': null,
-        'twitter:image': null,
-        'twitter:image:src': null,
-        'twitter:description': null,
-        'twitter:site': null,
-        'twitter:creator': null,
-      };
-      const meta = $('meta');
-      const $title = $('title');
-      const keys = Object.keys(meta);
-
-      let titleEl = $title.eq(0).text();
-
-      for (let s in data) {
-        keys.forEach((key) => {
-          if (meta[key].attribs
-            && (meta[key].attribs.property === s
-            || meta[key].attribs.name === s
-            || meta[key].attribs.itemprop === s)
-          ) {
-            data[s] = meta[key].attribs.content;
-          }
-        });
-      }
-
-      if (data.image && data.image.indexOf('http://') !== 0 &&
-          data.image.indexOf('https://') !== 0) {
-        data.image = responseUrl + data.image;
-      }
-
-      let description = null;
-      let title = null;
-      let image = null;
-
-      title = titleEl || data.title || data['og:title'] || data['twitter:title'];
-      description = data.description || data['og:description'] || data['twitter:description'];
-      image = data['og:image'] || data['og:image:url'] || data['twitter:image'] || data['og:image:src'] || data.image;
-
-      if (image && image.match(/^\/\//)) {
-        image = image.replace(/^\/\//, '');
-      }
-
-      let obj = {
-        image,
-        description,
-        title,
-        link
-      };
-
-      if (!image || !description || !title) {
-        console.log('url parse error');
-        console.log(data);
-      }
-
-      return obj;
-    })
-    .catch((error) => {
-      console.log(error, 'error');
-      return false;
-    });
+  if (!link.match('http://') && !link.match('https://')) {
+    link = 'http://' + link;
   }
+
+  // Handle youtube
+  if (link.indexOf('youtube') > -1) {
+    return getYoutubeLink(link);
+  }
+
+  return fetch(link, {
+    method: 'GET',
+    headers: link.match('apple.news') ? {} : fbHeader
+  })
+  .then((response) => {
+    responseUrl = response.url;
+    return response.text();
+  })
+  .then((responseTxt) => {
+    const $ = cheerio.load(responseTxt);
+
+    let redirect = $("meta[http-equiv='refresh']")[0];
+    let redirectUrl;
+
+    if (redirect && redirect.attribs && redirect.attribs.content) {
+      redirectUrl = redirect.attribs.content.split('URL=')[1];
+    }
+    if (redirectUrl) return generatePreview(redirectUrl);
+
+    let canonical = $("link[rel='canonical']")[0];
+    if (canonical && canonical.attribs) canonical = canonical.attribs;
+    else canonical = null;
+
+    if (canonical && extractDomain(canonical.href) !== extractDomain(link)) {
+      return generatePreview(canonical.href);
+    }
+
+    let data = {
+      title: null,
+      description: null,
+      image: null,
+      'og:type': null,
+      'og:title': null,
+      'og:description': null,
+      'og:image': null,
+      'og:image:url': null,
+      'og:url': null,
+      'al:web:url': null,
+      'twitter:title': null,
+      'twitter:image': null,
+      'twitter:image:src': null,
+      'twitter:description': null,
+      'twitter:site': null,
+      'twitter:creator': null,
+    };
+    const meta = $('meta');
+    const $title = $('title');
+    const keys = Object.keys(meta);
+
+    let titleEl = $title.eq(0).text();
+
+    for (let s in data) {
+      keys.forEach((key) => {
+        if (meta[key].attribs
+          && (meta[key].attribs.property === s
+          || meta[key].attribs.name === s
+          || meta[key].attribs.itemprop === s)
+        ) {
+          data[s] = meta[key].attribs.content;
+        }
+      });
+    }
+
+    if (data.image && data.image.indexOf('http://') !== 0 &&
+        data.image.indexOf('https://') !== 0) {
+      data.image = responseUrl + data.image;
+    }
+
+    let title = data.title || data['og:title'] || data['twitter:title'] || titleEl;
+    let description = data.description || data['og:description'] || data['twitter:description'];
+    let image = data['og:image'] || data['og:image:url'] || data['twitter:image'] || data['og:image:src'] || data.image;
+    let url = data['al:web:url'] || data['og:url'] || link;
+    let domain = extractDomain(url);
+
+    if (image && image.match(/^\/\//)) {
+      image = image.replace(/^\/\//, '');
+    }
+
+    let obj = {
+      image,
+      description,
+      title,
+      url,
+      domain
+    };
+
+    if (!image || !description || !title) {
+      console.log('url parse error');
+      console.log(data);
+    }
+
+    console.log($('head').html());
+
+    return obj;
+  })
+  .catch((error) => {
+    console.log(error, 'error');
+    return false;
+  });
 }
