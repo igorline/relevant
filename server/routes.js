302f1b5cb768679ba82f087e421cd4a67fbb5cec
Slava 4REAL <slava@4real.io>
2017-03-23 18:56:28 -0400

    https

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ import userController from './api/user/user.controller';
 let express = require('express');
 
 function requireHTTPS(req, res, next) {
-  if (!req.secure && process.env.NODE_ENV === 'production') {
+  if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
     return res.redirect('https://' + req.get('host') + req.url);
   }
   return next();
