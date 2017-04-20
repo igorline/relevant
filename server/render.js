94afb4dc9e31ac946528ca7f34df2d8b93a68d40
Slava 4REAL <slava@4real.io>
2017-04-20 01:25:10 -0400

    new img

--- server/render.js
+++ server/render.js
@@ -36,14 +36,14 @@ function renderFullPage(html, initialState) {
         />
         <meta property="og:title" content="Relevant: A Social News Reader" />
         <meta property="og:url" content="https://relevant.community" />
-        <meta property="og:image" content="https://relevant.community/img/fbfimg.jpg" />
+        <meta property="og:image" content="https://relevant.community/img/fbfimg.png" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@flickr" />
         <meta name="twitter:title" content="Relevant: A Social News Reader" />
         <meta name="twitter:description" content="
             Relevant promotes reliable information and rewards expertise. Instead of relying on quantity (# of likes, followers), Relevant’s algorithm relies on a quality metric - relevance score. This system is designed to penalise clickbait and fake news while promoting useful and reliable information." />
-        <meta name="twitter:image" content="https://relevant.community/img/fbfimg.jpg" />
+        <meta name="twitter:image" content="https://relevant.community/img/fbfimg.png" />
 
         ${styles}
       </head>
