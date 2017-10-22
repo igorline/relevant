ba6e6750d60c2b53a96622b35c34d2b7786c1846
Jules Laplace <julescarbon@gmail.com>
2017-10-22 16:47:29 +0200

    new userSuggestion component

--- app/web/components/common/avatarbox.component.js
+++ app/web/components/common/avatarbox.component.js
@@ -1,58 +1,56 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { numbers } from '../../../utils';
+import React from 'react';
 import { Link } from 'react-router';
+import { numbers } from '../../../utils';
 
 import Avatar from './avatar.component';
 
-export default class AvatarBox extends Component {
-  render() {
-    const user = this.props.user
-    const profileLink = '/profile/' + user._id;
-    let timestamp;
-    if (this.props.date) {
-      timestamp = ' • ' + numbers.timeSince(Date.parse(this.props.date)) + ' ago';
-    }
-    let premsg, className;
-    if (this.props.isRepost) {
-      className = 'repost';
-      premsg = 'reposted by ';
-    }
-    if (this.props.topic) {
-      timestamp = (
-        <span>
-          {' • '}
-          <img src='/img/r-emoji.png' className='r' />
-          {Math.round(this.props.topic.relevance)}
-          in
-          #{this.props.topic.name}
-        </span>
-      )
-    }
-    let relevance;
-    if (user.relevance && ! this.props.dontShowRelevance) {
-      relevance = (
-        <span>
-          <img src='/img/r-emoji.png' className='r' />
-          {Math.round(user.relevance)}
-        </span>
-      );
-    }
-    return (
-      <div className={['avatarBox', className].join(' ')}>
-        <Avatar user={user} />
-        <div className='userBox'>
-          <div className='bebasRegular username'>
-            {premsg}
-            <Link to={profileLink}>{user.name}</Link>
-            {relevance}
-          </div>
-          <div className='gray'>
-            @<Link to={profileLink}>{user._id}</Link>
-            {timestamp}
-          </div>
-        </div>
-      </div>
+export default function AvatarBox(props) {
+  const user = props.user;
+  const profileLink = '/profile/' + user._id;
+  let timestamp;
+  if (props.date) {
+    timestamp = ' • ' + numbers.timeSince(Date.parse(props.date)) + ' ago';
+  }
+  let premsg;
+  let className;
+  if (props.isRepost) {
+    className = 'repost';
+    premsg = 'reposted by ';
+  }
+  if (props.topic) {
+    timestamp = (
+      <span>
+        {' • '}
+        <img src="/img/r-emoji.png" alt="R" className="r" />
+        {Math.round(props.topic.relevance)}
+        in
+        #{props.topic.name}
+      </span>
     );
   }
+  let relevance;
+  if (user.relevance && !props.dontShowRelevance) {
+    relevance = (
+      <span>
+        <img src="/img/r-emoji.png" alt="R" className="R" />
+        {Math.round(user.relevance)}
+      </span>
+    );
+  }
+  return (
+    <div className={['avatarBox', className].join(' ')}>
+      <Avatar user={user} />
+      <div className="userBox">
+        <div className="bebasRegular username">
+          {premsg}
+          <Link to={profileLink}>{user.name}</Link>
+          {relevance}
+        </div>
+        <div className="gray">
+          @<Link to={profileLink}>{user._id}</Link>
+          {timestamp}
+        </div>
+      </div>
+    </div>
+  );
 }
