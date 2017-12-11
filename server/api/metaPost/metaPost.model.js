7d4312549c97b61039750eb84aab5358c8b0efa5
Slava 4REAL <slava@4real.io>
2017-12-11 14:25:15 -0500

    subscriptions

--- server/api/metaPost/metaPost.model.js
+++ server/api/metaPost/metaPost.model.js
@@ -37,18 +37,27 @@ let MetaPostSchema = new Schema({
   flaggedBy: [{ type: String, ref: 'User', select: false }],
   flaggedTime: Date,
 
+  twitterCommentary: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+  twitterScore: { type: Number, default: 0 },
+  tweetCount: { type: Number, default: 0 },
+  // TODO this could get big - should be separate table
+  seenInFeedNumber: { type: Number, default: 0 },
+  latestTweet: Date,
+  twitter: { type: Boolean, default: false },
+  feedRelevance: Number,
+
 }, {
   timestamps: true,
 });
 
 MetaPostSchema.index({ url: 1 });
 MetaPostSchema.index({ rank: 1 });
-MetaPostSchema.index({ latestPost: 1 });
+MetaPostSchema.index({ twitter: 1 });
 MetaPostSchema.index({ latestPost: 1 });
 MetaPostSchema.index({ latestPost: 1, tags: 1 });
 MetaPostSchema.index({ rank: 1, tags: 1 });
 
-MetaPostSchema.statics.updateRank = async function (_id) {
+MetaPostSchema.statics.updateRank = async function (_id, twitter) {
   let meta;
   try {
     meta = await this.findOne({ _id })
@@ -60,6 +69,10 @@ MetaPostSchema.statics.updateRank = async function (_id) {
     let highestRank = meta.commentary && meta.commentary.length ? meta.commentary[0].rank : 0;
     if (!meta.commentary || !meta.commentary.length) {
       // console.log(meta);
+    }
+    if (twitter && meta.twitter) {
+      meta.twitter = false;
+      meta.latestPost = new Date();
     }
     meta.rank = highestRank;
     meta = await meta.save();
