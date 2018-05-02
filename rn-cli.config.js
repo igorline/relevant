e317121fce8ac273e66f1b5b25034619fb44585c
Slava 4REAL <slava@4real.io>
2018-05-01 22:38:15 -0400

    upgrade

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,8 +1,7 @@
-// const blacklist = require('react-native/packager/blacklist');
 const blacklist = require('metro/src/blacklist');
 
 module.exports = {
   getBlacklistRE: function() {
-    return blacklist([/server\/.*/,/app\/web\/.*/]);
+    return blacklist([/server\/.*/, /app\/web\/.*/, /\.env/, /publicenv/]);
   }
 };
