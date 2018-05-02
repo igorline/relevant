bd8308462c911201cf3c82e49627056659240077
Slava 4REAL <slava@4real.io>
2018-05-01 22:26:05 -0400

    blacklist

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -2,6 +2,6 @@ const blacklist = require('react-native/packager/blacklist');
 
 module.exports = {
   getBlacklistRE: function() {
-    return blacklist([/server\/.*/,/app\/web\/.*/]);
+    return blacklist([/server\/.*/,/app\/web\/.*/,/\.env/,/publicenv/]);
   }
 };
