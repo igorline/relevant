347a2c30823a66ef82f1e4545901f600b6a8df8d
Slava 4REAL <slava@4real.io>
2018-05-23 12:59:40 -0400

    working upgrade

--- android/app/src/main/java/com/relevantnative/MainApplication.java
+++ android/app/src/main/java/com/relevantnative/MainApplication.java
@@ -20,29 +20,12 @@ import com.evollu.react.fa.FIRAnalyticsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.gnet.bottomsheet.RNBottomSheetPackage;
-import com.goldenowl.twittersignin.TwitterSigninPackage;
 import com.microsoft.codepush.react.CodePush;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.brentvatne.react.ReactVideoPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.horcrux.svg.SvgPackage;
-import com.meedan.ShareMenuPackage;
-import com.alinz.parkerdan.shareextension.SharePackage;
-import cl.json.RNSharePackage;
-import com.imagepicker.ImagePickerPackage;
-import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.evollu.react.fa.FIRAnalyticsPackage;
-import com.github.alinz.rnsk.RNSKPackage;
-import com.gnet.bottomsheet.RNBottomSheetPackage;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.github.yamill.orientation.OrientationPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -81,23 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
             new RNFetchBlobPackage(),
             new ReactNativeContacts(),
             new RNBottomSheetPackage(),
-          new TwitterSigninPackage(),
-          new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG), // Add/change this line.
-          new RNFetchBlobPackage(),
-          new ReactNativePushNotificationPackage(),
-          new VectorIconsPackage(),
-          new ReactVideoPackage(),
-          new LinearGradientPackage(),
-          new SvgPackage(),
-          new ShareMenuPackage(),
-          new SharePackage(),
-          new RNSharePackage(),
-          new ImagePickerPackage(),
-          new FIRAnalyticsPackage(),
-          new ReactNativeContacts(),
-          new RNSKPackage(),
-          new RNBottomSheetPackage(),
-          new OrientationPackage()
+            new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG) // Add/change this line.
         );
     }
 
