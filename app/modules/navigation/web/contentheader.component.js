6fdd833dd38d32e277f5612e21b505e136dee431
Santiago Angel <santiagoangel10@gmail.com>
2019-01-03 15:32:36 -0500

    Basic ContentHeader styles

--- app/modules/navigation/web/contentheader.component.js
+++ app/modules/navigation/web/contentheader.component.js
@@ -11,13 +11,40 @@ import DiscoverTabs from 'modules/discover/web/discoverTabs.component';
 import Activity from 'modules/activity/web/activity.container';
 import RequestInvite from 'modules/web_splash/requestInvite.component';
 import { matchPath } from 'react-router';
+import styled from 'styled-components';
 
-// const
+
+const Nav = styled.nav`
+  width: 100%;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SubNav = styled.div`
+  display: inline-block;
+  margin: 1em;
+`;
+
+const NavItem = styled.a`
+  color: blue;
+  margin: 0 1em;
+`;
+
 
 const ContentHeader = (props) => {
-  <nav>
-    hello
-  </nav>
+  return (
+    <Nav>
+      <SubNav>
+        <NavItem href="/new">New</NavItem>
+        <NavItem href="/trending">Trending</NavItem>
+      </SubNav>
+      <SubNav>
+        <NavItem href="/new">Activity</NavItem>
+        <NavItem href="/trending">New Post</NavItem>
+      </SubNav>
+    </Nav>
+  );
 };
 
 export default ContentHeader;
