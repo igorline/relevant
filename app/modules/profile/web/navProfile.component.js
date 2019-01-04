92d5e327b78fc710d6163a18b0f72b12200774a7
Santiago Angel <santiagoangel10@gmail.com>
2019-01-04 18:00:47 -0500

    Tidy up navProfile styles

--- app/modules/profile/web/navProfile.component.js
+++ app/modules/profile/web/navProfile.component.js
@@ -8,24 +8,35 @@ import { numbers } from 'app/utils';
 import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 
-const StyledIconImg = styled.img`
-  width: 30px;
-  height: 30px;
-  margin: 0 1em;
-`;
-
-const StyledAvatar = styled(Avatar)`
-  width: 64px;
-  margin: 0 1em;
-`;
 
 const ProfileContainer = styled.div`
   margin: 2em;
+  font-weight: bold;
+  font-size: 18px;
 `;
 
 const ProfileDetailsContainer = styled.div`
   display: flex;
   align-items: center;
+  margin-top: 1em;
+`;
+
+const StyledIconImg = styled.img`
+  width: 30px;
+  height: 30px;
+  margin: 0 0.3em 0 1em ;
+`;
+
+const StyledAvatar = styled(Avatar)`
+  background: red;
+  margin: 0 1em;
+`;
+
+
+const PendingPayouts = styled.div`
+  font-weight: normal;
+  color: hsl(0, 0%, 55%);
+  font-size: 14px;
 `;
 
 
@@ -59,15 +70,15 @@ class NavProfile extends Component {
     const id = this.props.handle;
     const user = this.props.user.users[id];
     if (!user) {
-      return <div className="profileContainer">User not found!</div>;
+      return null;
     }
 
     return (
       <ProfileContainer>
-        <h3>{user.name}</h3>
-        <p>PENDING PAYOUTS</p>
+        <div>{user.name}</div>
+        <PendingPayouts>PENDING PAYOUTS</PendingPayouts>
         <ProfileDetailsContainer>
-          <StyledAvatar user={user} />
+          <StyledAvatar user={user} size={64} />
           <StyledIconImg src="/img/r-emoji.png" alt="Relevance" />
           <span>{Math.round(user.relevance ? user.relevance.pagerank || 0 : 0)}</span>
           <StyledIconImg src="/img/relevantcoin.png" alt="Coins" />
