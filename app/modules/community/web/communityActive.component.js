dc5e0dcc69071b4e3d9dc29a15874179526a8892
Santiago Angel <santiagoangel10@gmail.com>
2019-01-09 13:45:52 -0500

    Improve layout of sidebar

--- app/modules/community/web/communityActive.component.js
+++ app/modules/community/web/communityActive.component.js
@@ -9,14 +9,17 @@ import styled, { css } from 'styled-components/primitives';
 import { colors } from 'app/styles/globalStyles';
 import ULink from 'modules/navigation/link.component';
 
-// const StyledULink = styled(ULink)`
-//   display: flex;
-//   align-items: center;
-//   color: ${colors.black};
-// `;
+const StyledULink = styled(ULink)`
+  display: flex;
+  align-items: center;
+  color: ${colors.black};
+  margin-right: 0.5em;
+`;
 
-const StyledView = styled.View`
-  margin-bottom: 1em;
+const NavSection = styled.View`
+  padding: 2em;
+  /* margin-bottom: 1em; */
+  border-bottom: 1px solid black;
 `;
 
 const MemberContainer = styled.View`
@@ -24,10 +27,28 @@ const MemberContainer = styled.View`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  font-weight: bold;
+  color: black;
 `;
 
 const StyledText = styled.Text`
   display: inline;
+`;
+
+const MemberCount = styled.Text`
+  font-weight: bold;
+  color: black;
+  margin-bottom: 1em;
+`;
+
+const StyledTopic = styled.Text`
+  margin-left: 1em;
+`;
+
+const MembersText = styled.Text`
+  display: inline;
+  border-bottom: solid black 1px;
+  padding: 1em 0;
 `;
 
 const StyledImage = styled.Image`
@@ -38,7 +59,6 @@ const StyledImage = styled.Image`
 `;
 
 const StyledCommunityList = styled.View`
-  margin: 2em;
 `;
 
 const linkStyles = {
@@ -48,12 +68,14 @@ const linkStyles = {
 const avatarStyles = {
   width: '30px',
   display: 'inline-block',
+  marginRight: '0.5em',
 };
 
 
 class CommunityActive extends Component {
   static propTypes = {
     community: PropTypes.object,
+    children: PropTypes.object,
     getCommunityMembers: PropTypes.func,
   };
 
@@ -62,31 +84,33 @@ class CommunityActive extends Component {
   }
 
   render() {
-    const { community } = this.props;
+    const { community, children } = this.props;
     const topics = get(community, 'topics', []);
     const members = get(community, 'members', []);
     const totalMembers = members.length;
     const limitedMembers = members.slice(0, 12);
     return (
       <StyledCommunityList>
-        {topics.map(topic => (
-          <ULink key={topic} to={`/${community.slug}/new/${topic}`} style={linkStyles}>
-            <StyledText >#{topic}</StyledText>
-          </ULink>
-        ))}
-        <StyledText>{
-          `${totalMembers} Members`
-        }</StyledText>
-        <MemberContainer>
-          {limitedMembers.map(member => (
-            <ULink key={member._id} to={`/user/profile/${member.user.handle}`} style={avatarStyles}>
-              <StyledImage source={{ uri: member.user.image }} />
+        <NavSection>
+          {children}
+          {topics.map(topic => (
+            <ULink key={topic} to={`/${community.slug}/new/${topic}`} style={linkStyles}>
+              <StyledTopic >#{topic}</StyledTopic>
             </ULink>
           ))}
-        </MemberContainer>
-
+        </NavSection>
+        <NavSection>
+          <MemberCount>{`${totalMembers} Members`}</MemberCount>
+          <MemberContainer>
+            {limitedMembers.map(member => (
+              <ULink key={member._id} to={`/user/profile/${member.user.handle}`} style={avatarStyles}>
+                <StyledImage source={{ uri: member.user.image }} />
+              </ULink>
+            ))}
+          </MemberContainer>
+        </NavSection>
       </StyledCommunityList>);
   }
-};
+}
 
 export default CommunityActive;
