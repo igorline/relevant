6f670f750646a325dc76f7b3950f1c2db4d6c869
Santiago Angel <santiagoangel10@gmail.com>
2019-01-09 13:50:28 -0500

    Add link to wallet in nav bar profile

--- app/modules/profile/web/navProfile.component.js
+++ app/modules/profile/web/navProfile.component.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { bindActionCreators } from 'redux';
 import get from 'lodash.get';
 import { connect } from 'react-redux';
 import Avatar from 'modules/user/web/avatar.component';
 import { numbers } from 'app/utils';
 import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
-
+import ULink from 'modules/navigation/link.component';
 
 const ProfileContainer = styled.div`
   margin: 2em;
@@ -38,6 +37,12 @@ const PendingPayouts = styled.div`
   color: hsl(0, 0%, 55%);
   font-size: 14px;
 `;
+
+const linkStyles = {
+  display: 'flex',
+  alignItems: 'center',
+  color: 'black',
+};
 
 
 class NavProfile extends Component {
@@ -79,10 +84,12 @@ class NavProfile extends Component {
         <PendingPayouts>PENDING PAYOUTS</PendingPayouts>
         <ProfileDetailsContainer>
           <StyledAvatar user={user} size={64} />
-          <StyledIconImg src="/img/r-emoji.png" alt="Relevance" />
-          <span>{Math.round(user.relevance ? user.relevance.pagerank || 0 : 0)}</span>
-          <StyledIconImg src="/img/relevantcoin.png" alt="Coins" />
-          <span>{fixed(this.state.tokens) || 0}</span>
+          <ULink to="/user/wallet" style={linkStyles}>
+            <StyledIconImg src="/img/r-emoji.png" alt="Relevance" />
+            <span>{Math.round(user.relevance ? user.relevance.pagerank || 0 : 0)}</span>
+            <StyledIconImg src="/img/relevantcoin.png" alt="Coins" />
+            <span>{fixed(this.state.tokens) || 0}</span>
+          </ULink>
         </ProfileDetailsContainer>
       </ProfileContainer>
     );
