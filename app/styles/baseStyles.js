549168b9a24f9cb51e45edd1140ea2042e430f71
Santiago Angel <santiagoangel10@gmail.com>
2019-01-22 15:02:06 -0500

    Create base styled Text components in accordance with style guide

---
+++ app/styles/baseStyles.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+import colors from 'app/styles/colors';
+
+// eslint-disable-next-line
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'HelveticaCondensed';
+    src: url('/fonts/HelveticaNeueCondensedBold.ttf');
+    font-weight: bold;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    font-size: 1.5rem;
+    line-height: 1.5;
+  }
+
+  html, body {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    color: ${colors.black};
+    font-family: Arial, sans-serif;
+    position:relative;
+    flex: 1;
+    -webkit-font-smoothing: antialiased;
+  }
+`;
