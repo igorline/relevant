16f5e8cb8673d5ca6be93a444484ca28c9c90fa6
Slava 4REAL <slava@4real.io>
2019-01-22 16:19:48 -0500

    react-native-web ssr

--- app/styles/globalStyles.js
+++ app/styles/globalStyles.js
@@ -5,17 +5,12 @@ import * as fonts from 'app/styles/fonts';
 
 // eslint-disable-next-line
 export const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'HelveticaCondensed';
-    src: url('/fonts/HelveticaNeueCondensedBold.ttf');
-    font-weight: bold;
-  }
-
   * {
     box-sizing: border-box;
   }
   html {
     font-size: 62.5%;
+    display: flex;
   }
   body {
     font-size: 1.5rem;
