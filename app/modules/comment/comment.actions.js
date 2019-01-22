7d2fa8c41d90ebf8d5d9b609cfc38fa5c4453c5d
Slava 4REAL <slava@4real.io>
2019-01-22 18:50:19 -0500

    comment actions pulled from post actions

---
+++ app/modules/comment/comment.actions.js
@@ -0,0 +1,95 @@
+import { normalize, schema } from 'normalizr';
+import { api } from 'app/utils';
+import * as types from 'core/actionTypes';
+import { setError } from 'modules/ui/error.actions';
+import { removePost } from 'modules/post/post.actions';
+
+const commentSchema = new schema.Entity('comments', {}, { idAttribute: '_id' });
+
+export function addComment(parentId, newComment) {
+  return {
+    type: types.ADD_COMMENT,
+    payload: {
+      comment: newComment,
+      parentId
+    }
+  };
+}
+
+export function setComments({ comments, childComments }) {
+  return {
+    type: types.SET_COMMENTS,
+    payload: {
+      comments,
+      childComments
+    }
+  };
+}
+
+function filterComments(comments) {
+  const childComments = {};
+  comments.forEach(c => {
+    if (!c.parentComment || c.parentComment === c.parentPost) {
+      return childComments[c.parentPost] = [...(childComments[c.parentPost] || []), c._id];
+    }
+    return childComments[c.parentComment] = [...(childComments[c.parentComment] || []), c._id];
+  });
+  return childComments;
+}
+
+export function createComment(commentObj) {
+  return async dispatch => {
+    try {
+      const comment = await api.request({
+        method: 'POST',
+        endpoint: 'comment',
+        path: '/',
+        body: JSON.stringify(commentObj)
+      });
+      const { parentComment, parentPost } = comment;
+      const parentId = !parentComment || parentComment === parentPost ? parentPost : parentComment;
+      dispatch(addComment(parentId, comment));
+      return true;
+    } catch (err) {
+      return false;
+    }
+  };
+}
+
+export function getComments(post, skip, limit) {
+  return async dispatch => {
+    try {
+      if (!skip) skip = 0;
+      if (!limit) limit = 0;
+
+      const responseJSON = await api.request({
+        method: 'GET',
+        endpoint: 'comment',
+        query: { post, skip, limit }
+      });
+
+      dispatch(setError('comments', false));
+      const childComments = filterComments(responseJSON.data);
+      const { comments } = normalize(responseJSON.data, [commentSchema]).entities;
+      dispatch(setComments({ comments, childComments }));
+    } catch (err) {
+      dispatch(setError('comments', true, err.message));
+    }
+  };
+}
+
+export function deleteComment(id) {
+  return async dispatch => {
+    try {
+      await api.request({
+        method: 'DELETE',
+        endpoint: 'comment',
+        path: '/' + id
+      });
+      return dispatch(removePost(id));
+    } catch (err) {
+      return false;
+    }
+  };
+}
+
