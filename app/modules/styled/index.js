34eb7e7a8b0897ea71a06b8c65693532dfe416cb
Slava 4REAL <slava@4real.io>
2019-01-29 15:56:23 -0500

    babel and user image fix

--- app/modules/styled/index.js
+++ app/modules/styled/index.js
@@ -1,6 +1,24 @@
 import styled from 'styled-components/primitives';
 import { mixins, sizing } from 'app/styles';
 
+export const View = styled.View`
+  ${mixins.margin}
+  ${mixins.height}
+  ${mixins.width}
+  ${mixins.padding}
+  ${mixins.background}
+  ${mixins.borderRadius}
+`;
+
+export const Text = styled.Text`
+  ${mixins.margin}
+  ${mixins.height}
+  ${mixins.width}
+  ${mixins.padding}
+  ${mixins.background}
+  ${mixins.borderRadius}
+`;
+
 export const Image = styled.Image`
   height: ${sizing(3)};
   width: ${sizing(3)};
