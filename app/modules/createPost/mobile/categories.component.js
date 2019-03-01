54ee9cb80aecb75e5d0142bbe657f1511cd87996
Santiago Angel <santiagoangel10@gmail.com>
2019-03-01 18:41:15 -0500

    User chooses from category and article tags when creating a post

--- app/modules/createPost/mobile/categories.component.js
+++ app/modules/createPost/mobile/categories.component.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import { View, InteractionManager } from 'react-native';
+import { InteractionManager, View } from 'react-native';
 import PropTypes from 'prop-types';
+import get from 'lodash.get';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as createPostActions from 'modules/createPost/createPost.actions';
 import * as tagActions from 'modules/tag/tag.actions';
-import Topics from './topics.component';
+// import Topics from './topics.component';
+import TagSelection from './tagSelection.component';
 
 class Categories extends Component {
   static propTypes = {
     createPost: PropTypes.object,
     actions: PropTypes.object,
-    tags: PropTypes.array
+    tags: PropTypes.array,
+    community: PropTypes.object
   };
 
   constructor(props, context) {
@@ -42,18 +45,21 @@ class Categories extends Component {
   }
 
   render() {
-    const { createPost, actions, tags } = this.props;
-    const { selectedTopic } = this;
+    const { createPost, actions, community } = this.props;
+    let communityTags = [];
+    if (community) {
+      const activeCommunity = get(community.communities, community.active, {}) || {};
+      communityTags = get(activeCommunity, 'topics', []);
+      // || [].map(t => t._id);
+    }
+    // const { selectedTopic } = this;
     let categoryEl;
 
     if (this.props.tags) {
       categoryEl = (
-        <Topics
-          ref={c => (this.topicsEl = c)}
-          topics={tags}
-          selectedTopic={selectedTopic}
-          type={'create'}
-          action={this.setTopic}
+        <TagSelection
+          topic={null}
+          communityTags={communityTags}
           actions={actions}
           createPost={createPost}
         />
@@ -71,6 +77,7 @@ class Categories extends Component {
 function mapStateToProps(state) {
   return {
     createPost: state.createPost,
+    community: state.community,
     user: state.user,
     tags: state.tags.parentTags
   };
@@ -81,7 +88,7 @@ function mapDispatchToProps(dispatch) {
     actions: bindActionCreators(
       {
         ...createPostActions,
-        ...tagActions,
+        ...tagActions
       },
       dispatch
     )
