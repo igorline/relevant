c67f34e135bd26dc2c07a2a175092edc917ff9f3
Santiago Angel <santiagoangel10@gmail.com>
2019-03-26 15:35:18 -0400

    Background changes on hover in community list.

--- app/modules/community/communityList.component.js
+++ app/modules/community/communityList.component.js
@@ -4,18 +4,20 @@ import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { joinCommunity } from 'community/community.actions';
+import styled from 'styled-components';
 
 import ULink from 'modules/navigation/ULink.component';
-import {
-  View,
-  Image,
-  BodyText,
-  Title,
-  LinkFont,
-  Header,
-  Divider
-} from 'modules/styled/uni';
+import { Image, BodyText, Title, LinkFont, Header, Divider } from 'modules/styled/uni';
+
+import { View } from 'modules/styled/web';
+
 import { colors } from 'app/styles';
+
+const CommunityItem = styled(View)`
+  :hover {
+    background: ${colors.secondaryBG};
+  }
+`;
 
 class CommunityAdminList extends Component {
   static propTypes = {
@@ -33,16 +35,16 @@ class CommunityAdminList extends Component {
       community: { communities }
     } = this.props;
     return (
-      <View m={'0 4'} fdirection="column">
-        <Header>Communities on Relevant</Header>
+      <View fdirection="column">
+        <Header m={'0 4'}>Communities on Relevant</Header>
         {Object.values(communities).map(c => {
           const communityURl = `/${c.slug}/new`;
           return (
-            <View fdirection="row" align="flex-start" mt={4} key={c._id}>
+            <CommunityItem fdirection="row" align="flex-start" p={'2 4 2 4'} key={c._id}>
               <ULink key={c.slug} to={communityURl}>
                 <Image source={c.image} h={8} w={8} mr={1} bg={colors.secondaryBG} />
               </ULink>
-              <View fdirection="column" h={8} justify="space-between" shrink={1}>
+              <View fdirection="column" justify="space-between" shrink={1}>
                 <View fdirection="row">
                   <BodyText inline={1}>
                     <ULink key={c.slug} to={communityURl} inline={1}>
@@ -64,19 +66,19 @@ class CommunityAdminList extends Component {
                     </ULink>
                   </BodyText>
                 </View>
-                <BodyText inline={1} c={colors.black} numberOfLines={1}>
+                <BodyText inline={1} c={colors.black}>
                   {c.description}
                 </BodyText>
                 <LinkFont inline={1} c={colors.black}>
                   {c.memberCount} member{c.memberCount > 1 ? 's' : ''}
                 </LinkFont>
               </View>
-            </View>
+            </CommunityItem>
           );
         })}
         <Divider m={'4 0'} />
-        <Header>Start a Community on Relevant</Header>
-        <BodyText m={'4 0 12 0'}>
+        <Header m={'0 4'}>Start a Community on Relevant</Header>
+        <BodyText m={'4 4 12 4'}>
           <ULink external to="mailto:info@relevant.community">
             Get in touch
           </ULink>{' '}
