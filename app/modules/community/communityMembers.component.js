683c0015d3cddfee8743b8d410437738ecf52f11
Santiago Angel <santiagoangel10@gmail.com>
2019-04-02 17:15:46 -0400

    Community members work with infinite scroll. Show reputation value. Reduce duplicate code in community members component

--- app/modules/community/communityMembers.component.js
+++ app/modules/community/communityMembers.component.js
@@ -2,28 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import {
   joinCommunity,
   searchMembers,
   getCommunityMembers
 } from 'community/community.actions';
 import AvatarBox from 'modules/user/avatarbox.component';
-import { connect } from 'react-redux';
-import { Input } from 'modules/styled/web';
 import AwesomeDebouncePromise from 'awesome-debounce-promise';
 import InfScroll from 'modules/listview/web/infScroll.component';
+import { Input } from 'modules/styled/web';
+import { View, SecondaryText } from 'modules/styled/uni';
+import styled from 'styled-components/primitives';
 
-import { View, BodyText, SecondaryText } from 'modules/styled/uni';
-
-const CommunityMember = ({ user }) => (
-  <View fdirection="row" m={['1 0']}>
-    <AvatarBox user={user} showRelevance condensedView={false} />
-  </View>
-);
-
-CommunityMember.propTypes = {
-  user: PropTypes.object
-};
+const ScrollContainer = styled(View)`
+  overflow: auto;
+`;
 
 class CommunityMembers extends Component {
   static propTypes = {
@@ -33,8 +27,7 @@ class CommunityMembers extends Component {
 
   constructor(props, context) {
     super(props, context);
-    // TODO should set it here and not on server
-    this.pageSize = 2;
+    this.pageSize = 20;
     this.hasMore = true;
     this.ready = false;
     this.role = null;
@@ -86,19 +79,24 @@ class CommunityMembers extends Component {
     return TITLES[role];
   }
 
-  renderRow = memberId => {
-    const { community } = this.props;
-    const { members } = community;
-    const user = members[memberId];
+  renderRow = user => {
     const title = this.role === user.role ? null : this.getTitle(user.role);
     this.role = user.role;
     return (
       <React.Fragment key={user._id}>
         {title ? <SecondaryText m={'2 0'}>{title}</SecondaryText> : null}
-        <CommunityMember
-          user={{ ...user.embeddedUser, relevance: user.reputation + 0.1 }}
-          key={user._id}
-        />
+        <View fdirection="row" m={['1 0']}>
+          <AvatarBox
+            user={{
+              ...user.embeddedUser,
+              relevance: {
+                pagerank: user.reputation
+              }
+            }}
+            showRelevance
+            condensedView={false}
+          />
+        </View>
       </React.Fragment>
     );
   };
@@ -106,71 +104,43 @@ class CommunityMembers extends Component {
   render() {
     const { community } = this.props;
     const { searchResults, searchValue } = this.state;
-    const { active, communityMembers } = community;
+    const { active, members, communityMembers } = community;
     const activeCommunityMembers = communityMembers[active] || [];
-    const rows = activeCommunityMembers.map(a => this.renderRow(a));
-    // const admins = activeCommunityMembers.filter(member => members[member].role === 'admin');
-    // const others = activeCommunityMembers.filter(member => admins.indexOf(member) === -1);
     return (
       <View fdirection="column">
-        <BodyText>Search bar</BodyText>
         <Input
           placeholder="Search"
           onChange={this.handleChange}
           value={searchValue}
           type="search"
         />
-        <InfScroll
-          className={'communityMembers'}
-          data={activeCommunityMembers}
-          loadMore={p => this.load(p, activeCommunityMembers.length)}
-          hasMore={this.hasMore}
-          useWindow
-        >
-          {rows}
-        </InfScroll>
-        <View mt={2}>
-          {!!searchValue && searchResults.length
-            ? searchResults.map(user => {
-              const title = this.role === user.role ? null : this.getTitle(user.role);
-              this.role = user.role;
-              return (
-                <React.Fragment key={user._id}>
-                  {title ? <SecondaryText m={'2 0'}>{title}</SecondaryText> : null}
-                  <CommunityMember
-                    user={{ ...user.embeddedUser, relevance: user.reputation + 0.1 }}
-                    key={user._id}
-                  />
-                </React.Fragment>
-              );
-            })
-            : null}
-        </View>
+
+        <ScrollContainer h={40} mt={2}>
+          {!searchValue ? (
+            <InfScroll
+              className={'communityMembers'}
+              data={activeCommunityMembers}
+              loadMore={p => this.load(p, activeCommunityMembers.length)}
+              hasMore={this.hasMore}
+              useWindow={false}
+            >
+              {activeCommunityMembers.map(memberId => {
+                const user = members[memberId];
+                return this.renderRow(user);
+              })}
+            </InfScroll>
+          ) : (
+            searchResults.map(user => this.renderRow(user))
+          )}
+        </ScrollContainer>
       </View>
     );
   }
 }
 
-// {!searchValue &&
-//   activeCommunityMembers.map(memberId => {
-//     const user = members[memberId];
-//     const title = this.role === user.role ? null : this.getTitle(user.role);
-//     this.role = user.role;
-//     return (
-//       <React.Fragment key={user._id}>
-//         {title ? <SecondaryText m={'2 0'}>{title}</SecondaryText> : null}
-//         <CommunityMember
-//           user={{ ...user.embeddedUser, relevance: user.reputation + 0.1 }}
-//           key={user._id}
-//         />
-//       </React.Fragment>
-//     );
-//   })}
-
 const mapStateToProps = state => ({
   routing: state.routing,
   community: state.community
-  // auth: state.auth
 });
 
 const mapDispatchToProps = dispatch => ({
