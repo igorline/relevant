eabf6817cc01855739692fa078338b4aaba0dbf8
Slava 4REAL <slava@4real.io>
2019-05-29 17:23:09 -0400

    fix for % in link

--- app/modules/discover/web/discoverTabs.component.js
+++ app/modules/discover/web/discoverTabs.component.js
@@ -9,11 +9,10 @@ import { standardRoutes } from './discoverHelper';
 const DiscoverTab = props => {
   const { linkData, community, tag } = props;
   let url = `/${community}/${linkData.key}`;
-  if (tag) {
-    url += `/${tag}`;
-  }
+  if (tag) url += `/${tag}`;
+
   return (
-    <StyledNavLink to={url} mr={[2, 1]}>
+    <StyledNavLink to={encodeURI(url)} mr={[2, 1]}>
       {linkData.title}
     </StyledNavLink>
   );
