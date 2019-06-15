8b150c62ca0e4fa89945ece0355f0e2cea14c92b
sethfork <sefeibus@gmail.com>
2019-06-15 01:04:51 -0400

    Get claimTokens working; refactor wallet

--- app/modules/wallet/web/cashOutModal.component.js
+++ app/modules/wallet/web/cashOutModal.component.js
@@ -1,54 +1,40 @@
-import { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import crypto from 'crypto';
 import Modal from 'modules/ui/web/modal';
 import { alert } from 'app/utils';
 import { Button, View, BodyText, SecondaryText } from 'modules/styled/uni';
+import ContractProvider, { contractPropTypes } from 'modules/contract/contract.container';
+import {
+  getProvider,
+  formatBalanceWrite,
+  generateSalt
+} from 'modules/web_ethTools/utils';
 
 const Alert = alert.Alert();
+const web3 = getProvider();
+const decimals = 18;
 
-export default class AddEthAddress extends Component {
-  static propTypes = {
-    account: PropTypes.string,
-    actions: PropTypes.object,
-    balance: PropTypes.number,
-    modal: PropTypes.string,
-    contract: PropTypes.object,
-    user: PropTypes.object
-    // connectedAccount: PropTypes.string
-  };
-
-  cashOut = async () => {
-    const { actions, user, contract } = this.props;
+const AddEthAddress = ({ actions, user, accounts, modal, balance, cacheSend }) => {
+  const _cashOut = async () => {
     try {
-      // TODO call await this.connectAddress() here if no ethAddress is present
-
-      const decimals = contract.methods.decimals.cacheCall();
+      if (!user.ethAddress.length) await connectAddress();
 
       let cashOut = await actions.cashOut();
       cashOut = cashOut || user.cashOut;
+      const { sig, amount: _amount } = cashOut;
+      const amount = formatBalanceWrite(_amount, decimals);
 
-      const { sig } = cashOut;
-      let amount = new web3.utils.BN(cashOut.amount.toString());
-      let mult = new web3.utils.BN(10 ** (decimals / 2));
-      mult = mult.mul(mult);
-      amount = amount.mul(mult);
-
-      // let result = await this.props.RelevantCoin.methods.cashOut(amount, sig).call();
-      // console.log(result);
-      contract.methods.cashOut.cacheSend(amount, sig, {
-        from: user.ethAddress[0]
-      });
-      // console.log(result);
+      if (accounts[0]) {
+        cacheSend('claimTokens', { from: accounts[0] }, amount, sig);
+      }
     } catch (err) {
       throw err;
     }
   };
 
-  connectAddress = async () => {
-    const { actions, account } = this.props;
+  const connectAddress = async () => {
     try {
-      const salt = crypto.randomBytes(16).toString('hex');
+      const salt = generateSalt();
       const msgParams = [
         {
           type: 'string',
@@ -56,15 +42,20 @@ export default class AddEthAddress extends Component {
           value: 'Connect Ethereum address to the Relevant account ' + salt
         }
       ];
-      web3.currentProvider.sendAsync(
+
+      await web3.currentProvider.connection.sendAsync(
         {
           method: 'eth_signTypedData',
-          params: [msgParams, account],
-          from: account
+          params: [msgParams, accounts[0]],
+          from: accounts[0]
         },
         (err, msg) => {
-          if (err || msg.error) return;
-          actions.addEthAddress(msgParams, msg.result, account);
+          if (err || msg.error) {
+            // eslint-disable-next-line no-console
+            console.error('Error: ', err || msg.error);
+            return;
+          }
+          actions.addEthAddress(msgParams, msg.result, accounts[0]);
         }
       );
     } catch (err) {
@@ -72,43 +63,53 @@ export default class AddEthAddress extends Component {
     }
   };
 
-  componentDidUpdate(prevProps) {
-    if (!prevProps.balance && this.props.balance > 0 && this.state.modal === false) {
-      this.setState({ modal: true });
+  const [_modal, setModal] = useState(false);
+  useEffect(() => {
+    if (!balance || (balance > 0 && _modal === false)) {
+      setModal(true);
     }
-  }
+  }, [balance]);
 
-  render() {
-    const { actions, modal } = this.props;
-    return (
-      <Modal
-        visible={modal === 'cashOut'}
-        close={actions.hideModal}
-        title={'Claim Your Relevant Coins'}
-      >
-        <View>
-          <BodyText>
-            This will transfer the coins you have to your Ethereum wallet.
-          </BodyText>
-          <BodyText mt={2}>TODO: show prompt to install Metamask if no Metamask</BodyText>
-          <SecondaryText>
-            Like this: *We'll need to connect your account, it is not a transaction and is
-            totally free
-          </SecondaryText>
-          <BodyText>
-            TODO: explain if no account is connected to db (prompt to sign tx)
-          </BodyText>
-          <BodyText>TODO: warning if network mismatch</BodyText>
-          <BodyText>TODO: warning if current account mismatch</BodyText>
-          <BodyText>TODO: warning if current account mismatch</BodyText>
-          <BodyText>TODO: add custom amount?</BodyText>
-          <BodyText>TODO: add custom address?</BodyText>
+  return (
+    <Modal
+      visible={modal === 'cashOut'}
+      close={actions.hideModal}
+      title={'Claim Your Relevant Coins'}
+    >
+      <View>
+        <BodyText>
+          This will transfer the coins you have to your Ethereum wallet.
+        </BodyText>
+        <BodyText mt={2}>TODO: show prompt to install Metamask if no Metamask</BodyText>
+        <SecondaryText>
+          Like this: *We'll need to connect your account, it is not a transaction and is
+          totally free
+        </SecondaryText>
+        <BodyText>
+          TODO: explain if no account is connected to db (prompt to sign tx)
+        </BodyText>
+        <BodyText>TODO: warning if network mismatch</BodyText>
+        <BodyText>TODO: warning if current account mismatch</BodyText>
+        <BodyText>TODO: warning if current account mismatch</BodyText>
+        <BodyText>TODO: add custom amount?</BodyText>
+        <BodyText>TODO: add custom address?</BodyText>
 
-          <Button mr={'auto'} mt={4} onClick={this.cashOut}>
-            Claim Relevant Coins
-          </Button>
-        </View>
-      </Modal>
-    );
-  }
-}
+        <Button mr={'auto'} mt={4} onClick={() => _cashOut()}>
+          Claim Relevant Coins
+        </Button>
+      </View>
+    </Modal>
+  );
+};
+
+AddEthAddress.propTypes = {
+  ...contractPropTypes,
+  actions: PropTypes.object,
+  balance: PropTypes.number,
+  modal: PropTypes.string,
+  user: PropTypes.object
+};
+
+export default ContractProvider(AddEthAddress);
+
+// To allacote Rewards -- cacheSend('allocateRewards', { from: accounts[0] }, rewardsToAllocate);
