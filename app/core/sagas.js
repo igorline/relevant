5a85a5db368dce25d4d6e9b8da883cb6155ea692
sethfork <sefeibus@gmail.com>
2019-06-15 01:04:33 -0400

    Spawn RelevantToken saga as detatched thread

--- app/core/sagas.js
+++ app/core/sagas.js
@@ -1,10 +1,10 @@
-import { all, fork } from 'redux-saga/effects';
+import { all, fork, spawn } from 'redux-saga/effects';
 import { sagas as statesauceSagas } from 'redux-saga-web3';
 import { saga as RelevantToken } from './contracts';
 
 export default function* rootSaga() {
   yield all([
     ...Object.values(statesauceSagas).map(saga => fork(saga)),
-    fork(RelevantToken)
+    spawn(RelevantToken)
   ]);
 }
