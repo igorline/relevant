a018739c7d59005a4002e1f2d94e01d4298b6cdb
Slava 4REAL <slava@4real.io>
2019-10-30 23:43:05 -0400

    no rewards for inactive communities

--- server/utils/ethRewards.js
+++ server/utils/ethRewards.js
@@ -36,7 +36,7 @@ exports.rewards = async () => {
   }
 
   try {
-    const communities = await Community.find({});
+    const communities = await Community.find({ inactive: { $ne: true } });
 
     // const stakedTokens = await Community.getBalances();
     const stakedTokens = await Earnings.stakedTokens();
