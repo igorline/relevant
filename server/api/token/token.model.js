3321fa0ab4b634e3e877c82c1708ae4fc0db785d
Slava 4REAL <slava@4real.io>
2019-11-13 14:52:25 -0500

    fix auth token store

--- server/api/token/token.model.js
+++ server/api/token/token.model.js
@@ -15,12 +15,12 @@ const AuthTokenSchema = new Schema(
 AuthTokenSchema.statics.revoke = async function revoke(_id) {
   const now = new Date();
   const revokeBefore = Math.floor(now.getTime() / 1000);
-  const revokeToken = new this({ _id, revokeBefore });
+  const revokeToken = new this({ user: _id, revokeBefore });
   return revokeToken.save();
 };
 
 AuthTokenSchema.statics.checkRevoked = async function checkRevoked({ _id, iat }) {
-  return this.findOne({ _id, revokeBefore: { $gte: iat } });
+  return this.findOne({ user: _id, revokeBefore: { $gte: iat } });
 };
 
 module.exports = mongoose.model('AuthToken', AuthTokenSchema);
