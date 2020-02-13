54cab196de0f92255688d64d4199e54eeba7299e
Slava 4REAL <slava@4real.io>
2020-02-13 17:32:29 -0500

    update banner test

--- app/modules/navigation/__test__/banner.test.js
+++ app/modules/navigation/__test__/banner.test.js
@@ -8,6 +8,17 @@ const props = {
   cta: 'SIGN_UP'
 };
 
+jest.mock('react-redux', () => {
+  const state = {
+    navigation: { screenSize: 0 }
+  };
+  return {
+    useDispatch: () => {},
+    connect: el => el,
+    useSelector: fn => fn(state)
+  };
+});
+
 test('Snapshot SplashComponent', () => {
   const tree = renderer
     .create(
