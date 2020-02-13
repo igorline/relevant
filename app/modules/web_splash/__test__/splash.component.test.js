6565d16c0927f67395a974bb41e7047d5633af2d
Slava 4REAL <slava@4real.io>
2020-02-13 01:46:15 -0500

    update splash test

--- app/modules/web_splash/__test__/splash.component.test.js
+++ app/modules/web_splash/__test__/splash.component.test.js
@@ -8,13 +8,20 @@ const props = {
   cta: 'SIGN_UP'
 };
 
+jest.mock('react-redux', () => {
+  return {
+    useDispatch: () => {},
+    connect: el => el
+  };
+});
+
 test('Snapshot SplashComponent', () => {
   const tree = renderer
-  .create(
-    <MemoryRouter>
-      <SplashComponent {...props} overRideDismiss />
-    </MemoryRouter>
-  )
-  .toJSON();
+    .create(
+      <MemoryRouter>
+        <SplashComponent {...props} overRideDismiss />
+      </MemoryRouter>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
